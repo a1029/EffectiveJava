233ebdfc04130d3769d48bec27bb8aac5b743982
J065_김한빈 <rttr1023@gmail.com>
2022-02-04 22:20:14 +0900

    아이템 3 - Serializable

--- src/main/java/item3/Elvis2.java
+++ src/main/java/item3/Elvis2.java
@@ -1,6 +1,8 @@
 package item3;
 
-public class Elvis2 {
+import java.io.Serializable;
+
+public class Elvis2 implements Serializable {
 
     private static final Elvis2 INSTANCE = new Elvis2();
 
@@ -8,6 +10,10 @@ public class Elvis2 {
         if (INSTANCE != null){
             throw new RuntimeException("이미 인스턴스가 존재합니다");
         }
+    }
+
+    private Object readResolve() {
+        return INSTANCE;
     }
 
     public static Elvis2 getInstance(){
